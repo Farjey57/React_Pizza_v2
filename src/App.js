0bbf576021795a747678ae2ac3bab2b88e03e203
work_sist <cvet57@rambler.ru>
2023-10-30 17:23:10 +0300

    пятый урок. Бэкэенд, fetch

--- src/App.js
+++ src/App.js
@@ -9,23 +9,28 @@ import pizzas from './assets/pizza.json';
 
 import './scss/app.scss';
 
-console.log(pizzas);
+
 
 function App() {
+  //https://653fb6cc45bedb25bfc1163e.mockapi.io/items
+
+  fetch();
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            {/* <Categories /> */}
+            {<Categories />}
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {pizzas.map((obj) => (
               // {...obj} - если ключи такие же точно будут
-              <PizzaBlock key={obj.id}
+              <PizzaBlock
+                key={obj.id}
                 title={obj.title}
                 price={obj.price}
                 image={obj.imageUrl}

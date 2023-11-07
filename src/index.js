722290ba602522c2a4beff65c1e8871eca130e98
work_sist <cvet57@rambler.ru>
2023-11-07 17:31:45 +0300

    12 урок Redux

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css'; //Просто запусти код из пути
 import App from './App';
 
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
 const root = ReactDOM.createRoot(document.getElementById('root')); //Точка входа для размещения реакт кода
 
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 ); //отрисовка содержимого <App>
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../redux/slices/filterSlice';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+console.log(store);
